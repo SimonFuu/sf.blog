490d7b488892f54f41194b6f0e0aacb3ea1993f9
Simon Fu <fushupeng@outlook.com>
2017-10-16 19:04:54 +0800

    edit common layout

--- resources/views/layouts/layouts.blade.php
+++ resources/views/layouts/layouts.blade.php
@@ -1,11 +1,13 @@
 <!doctype html>
 <html lang="{{ app()->getLocale() }}">
 <head>
+    <title>{{ env('APP_NAME') }}</title>
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>jjjjjjjj</title>
+    <meta name="keywords" content="">
+    <meta name="description" content="">
     <link rel="stylesheet" href="{{ env('APP_STATIC_FILE_SERVER') }}/css/app.css">
     <link rel="stylesheet" href="{{ env('APP_STATIC_FILE_SERVER') }}/plugins/font-awesome-4.7.0/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ env('APP_STATIC_FILE_SERVER') }}/css/style.css?v={{ env('APP_DEBUG') ? time() : env('APP_STATIC_FILE_VERSION') }}">
