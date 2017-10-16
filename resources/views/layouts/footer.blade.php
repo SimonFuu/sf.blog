1286831ccb9856c9acfe1233c57e15ab54c67401
Simon Fu <fushupeng@outlook.com>
2017-10-17 02:20:18 +0800

    add upyun logo

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,7 @@
-<footer class="text-center" style="padding-bottom: 20px">
+<footer class="text-center">
     &copy; {{ date('Y') }} <a href="/about">Simon Fu</a> | <a href="http://www.miibeian.gov.cn/" target="_blank">蜀ICP备17016665号-1</a>
+    <div>
+        <span>CDN服务提供商</span>
+        <a href="https://console.upyun.com/register/?invite=SJF8aPz6-" target="_blank"><img src="/storage/images/upyun.png" alt="" height="40"></a>
+    </div>
 </footer>
