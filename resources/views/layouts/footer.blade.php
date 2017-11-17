4c6bb333420944a9d044d36e2528211b3f02bf11
Simon Fu <fushupeng@outlook.com>
2017-11-17 16:28:07 +0800

    change upyun url

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -2,6 +2,6 @@
     <p></p>&copy; {{ date('Y') }} <a href="{{ route('index') }}">Simon Fu</a> | <a href="http://www.miibeian.gov.cn/" target="_blank">{{ Cache::get('SETTINGS')['ICP_NUMBER'] }}</a>
     <p>
         <span>CDN服务提供商</span>
-        <a href="https://console.upyun.com/register/?invite=SJF8aPz6-" target="_blank"><img src="{{ config('app.storage_host') }}/storage/images/common/upyun.png" alt="" height="40"></a>
+        <a href="https://www.upyun.com" target="_blank"><img src="{{ config('app.storage_host') }}/storage/images/common/upyun.png" alt="" height="40"></a>
     </p>
 </footer>
